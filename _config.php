cec6bc567d6625fd42ea6bc103946d621f7a4d8a
ommishra1233 <99781441+ommishra1233@users.noreply.github.com>
2023-03-27 11:44:31 +0530

    _config.php

--- _config.php
+++ _config.php
@@ -22,7 +22,7 @@ $disqus = "https://indianime.disqus.com"; // Disqus
 
 
 // API URL
-$api = "http://anikatsu.shashanktiwar11.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
+$api = "https://api.animedex.live/"; // https://github.com/shashankktiwariii/anikatsu-api
 
 
 $banner = $websiteUrl . "/files/images/banner.png";  //Banner
